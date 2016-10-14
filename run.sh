14fbf2c6d59b67931e61e836a8b16a05b2d7e021
杨文轩 <gzyangwenxuan@corp.netease.com>
2016-10-14 17:38:25 +0800

    add broker and self ip var

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+IP=1.1.1.1
+BROKER_IP=2.2.2.2
+
 function usage(){
 	echo "$0 kiil"
 	echo "$0 run process_num base_port"
@@ -23,13 +26,13 @@ if [ $1 = 'run' ]; then
 	proc_num=$2
 	base_port=$3
 	for port in $(seq $base_port 2 $((base_port + (proc_num-1)*2))); do
-		python zeromq_bench.py --host 10.130.18.223 --port 19600 --bin --ip ${IP} --recv-port $port --send-port $((port+1)) > "bench_${port}.log"
+		python zeromq_bench.py --host $BROKER_IP --port 19600 --bin --ip ${IP} --recv-port $port --send-port $((port+1)) > "bench_${port}.log"
 	done
 elif [ $1 = 'runss' ]; then
 	proc_num=$2
 	base_port=$3
 	for port in $(seq base_port 1 $((base_port + proc_num - 1))); do
-		python zeromq_bench.py --host 10.130.18.223 --port 19600 --bin --ip ${IP} --my-port $port > "bench_${port}.log"
+		python zeromq_bench.py --host $BROKER_IP --port 19600 --bin --ip ${IP} --my-port $port > "bench_${port}.log"
 	done
 else
 	usage
