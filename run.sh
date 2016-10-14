c2c2fb02138fb2e93c54b25ef8625c5905193131
杨文轩 <gzyangwenxuan@corp.netease.com>
2016-10-14 17:42:16 +0800

    bash bugfix

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ BROKER_IP=2.2.2.2
 function usage(){
 	echo "$0 kiil"
 	echo "$0 run process_num base_port"
-	echo "$0 runss process_num base_port"
+	echo "$0 runrr process_num base_port"
 }
 
 if [ $# -lt 1 ]; then
@@ -28,11 +28,11 @@ if [ $1 = 'run' ]; then
 	for port in $(seq $base_port 2 $((base_port + (proc_num-1)*2))); do
 		python zeromq_bench.py --host $BROKER_IP --port 19600 --bin --ip ${IP} --recv-port $port --send-port $((port+1)) > "bench_${port}.log"
 	done
-elif [ $1 = 'runss' ]; then
+elif [ $1 = 'runrr' ]; then
 	proc_num=$2
 	base_port=$3
 	for port in $(seq $base_port 1 $((base_port + proc_num - 1))); do
-		python zeromq_ss_bench.py --host $BROKER_IP --port 19600 --bin --ip ${IP} --my-port $port > "bench_${port}.log"
+		python zeromq_rr_bench.py --host $BROKER_IP --port 19600 --bin --ip ${IP} --my-port $port > "bench_${port}.log"
 	done
 else
 	usage
